416631e5169d48aba7a560793dd4efc41381f512
Jim Disenstam <jim.disenstam@gmail.com>
2018-11-29 09:59:03 +0100

    Getting post answer working

--- src/js/quiz-game.js
+++ src/js/quiz-game.js
@@ -21,21 +21,35 @@ export class QuizGame extends window.HTMLElement {
     this.shadowRoot.appendChild(template.content.cloneNode(true))
     this._quizCard = this.shadowRoot.querySelector('#quiz-card')
     this.question = null
+    this.answer = '2'
   }
   connectedCallback () {
     this._quizCard.textContent = 'Hello World again!'
     this.getQuestion()
+    this.sendAnswer()
   }
 
-  async getNextQuestion () {
+  async sendAnswer () {
+    let response = await this.postData(`http://vhost3.lnu.se:20080/answer/1`, { answer: 2 })
+    console.log(response)
+  }
 
+  async postData (url, data) {
+    let response = await window.fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+    response = await response.json()
+    return response
   }
 
   async getQuestion () {
     let response = await window.fetch(`http://vhost3.lnu.se:20080/question/1`)
     response = await response.json()
     this.question = response.question
-    console.log(this.question)
   }
 }
 
