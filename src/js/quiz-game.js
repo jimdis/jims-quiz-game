5d21153b58e99bd3bf375e45574c74c53b119369
Jim Disenstam <jim.disenstam@gmail.com>
2018-11-30 12:42:28 +0100

    Getting quiz working to end

--- src/js/quiz-game.js
+++ src/js/quiz-game.js
@@ -10,7 +10,7 @@ template.innerHTML = /* html */`
 <div id="quiz-card">
 <h3 id="question"></h3>
 <label for="answer">Your answer:</label>
-<input type="number" id="input-number" name ="answer">
+<input type="text" id="input-number" name ="answer">
 <button id="button">Send Answer</button>
 <h3 id="answer"></h3>
 </div>
@@ -30,52 +30,48 @@ class QuizGame extends window.HTMLElement {
     this.shadowRoot.appendChild(template.content.cloneNode(true))
     this.apiURL = 'http://vhost3.lnu.se:20080/question/1'
     this._quizCard = this.shadowRoot.querySelector('#quiz-card')
+    this.response = null
     this.question = null
     this.questionID = 1
   }
   connectedCallback () {
+    this._quizCard.querySelector('#button').addEventListener('click', async () => {
+      let answer = this.getAnswer()
+      console.log('Answer submitted: ' + answer)
+      this.response = await this.postData(this.apiURL, { answer: answer })
+      this.apiURL = this.response.nextURL
+      this._updateRendering()
+    })
     this.getQuestion()
   }
 
   async getQuestion () {
     let response = await window.fetch(this.apiURL)
-    response = await response.json()
-    console.log(response)
-    this.question = response.question
-    this.apiURL = response.nextURL
-    this._updateRendering(response)
-    this.getAnswer(response)
+    this.response = await response.json()
+    this.question = this.response.question
+    this.apiURL = this.response.nextURL
+    this._updateRendering()
   }
 
-  searchAnswer (response) {
+  getAnswer () {
     let answer = 'ANSWER NOT CHANGED!'
-    if (response.question && !response.alternatives) {
+    if (this.response.question && !this.response.alternatives) {
       console.log('FIRST IF ACTIVE!')
       let input = this._quizCard.querySelector('#input-number')
       answer = input.value
       console.log('Answer changed in first if to: ' + answer)
     }
-    if (response.alternatives) {
+    if (this.response.alternatives) {
       console.log('SECOND IF ACTIVE!')
       let alternatives = this._quizCard.querySelectorAll('[type=radio]')
       for (let alternative of Array.from(alternatives)) {
-        alternative.addEventListener('change', () => {
+        if (alternative.checked) {
           answer = alternative.value
-          console.log('Answer changed in second if to: ' + answer)
-        })
+        }
       }
+      console.log('Answer changed in second if to: ' + answer)
     }
     return answer
-  }
-  getAnswer (response) {
-    this._quizCard.querySelector('#button').addEventListener('click', async () => {
-      let answer = this.searchAnswer(response)
-      console.log('Answer submitted: ' + answer)
-      let nextResponse = await this.postData(this.apiURL, { answer: answer })
-      console.log(nextResponse)
-      this.apiURL = nextResponse.nextURL
-      this._updateRendering(nextResponse)
-    })
   }
 
   async postData (url, data) {
@@ -91,17 +87,19 @@ class QuizGame extends window.HTMLElement {
     return response
   }
 
-  _updateRendering (response) {
+  _updateRendering () {
     let question = this._quizCard.querySelector('#question')
     let answer = this._quizCard.querySelector('#answer')
-    if (response.question && !response.alternatives) {
-      console.log('textQuestion')
+    console.log(this.response)
+    if (this.response.question && !this.response.alternatives) {
+      console.log('THIS IS INTERPRETED AS A TEXT QUESTION!')
     }
-    if (response.alternatives) {
-      Object.keys(response.alternatives).forEach((key) => { // GÅR NEDAN ATT GÖRA ENKLARE MED EN TEMPLATE?
+    if (this.response.alternatives) {
+      console.log('THIS IS INTERPRETED AS AN ALTERNATIVES QUESTION!')
+      Object.keys(this.response.alternatives).forEach((key) => { // GÅR NEDAN ATT GÖRA ENKLARE MED EN TEMPLATE?
         let label = document.createElement('label')
         let radioButton = document.createElement('input')
-        let text = document.createTextNode(response.alternatives[key])
+        let text = document.createTextNode(this.response.alternatives[key])
         radioButton.setAttribute('name', 'alt')
         radioButton.setAttribute('type', 'radio')
         radioButton.setAttribute('value', key)
@@ -112,8 +110,8 @@ class QuizGame extends window.HTMLElement {
     }
     question.textContent = this.question
     answer.textContent = ''
-    if (!response.question) {
-      answer.textContent = response.message
+    if (!this.response.question) {
+      answer.textContent = this.response.message
       setTimeout(() => { this.getQuestion() }, 1000)
     }
   }
