bfead2b8cbbabb86d13fa5f8e42847dabafcfdf4
Jim Disenstam <jim.disenstam@gmail.com>
2018-12-05 14:11:23 +0100

    Updating rendering function, adding styling

--- src/js/jims-quiz-game.js
+++ src/js/jims-quiz-game.js
@@ -6,19 +6,61 @@ template.innerHTML = /* html */`
 <style>
   :host {
     display: block;
-    width: 50%;
+    width: 100%;
     margin: 10px 0 10px;
     border: 1px solid grey;
+    background-color: #f6f7f7;
+    font-family: "Lucida Grande", sans-serif;
+    -webkit-font-smoothing: antialiased;
+    font-size: 100%;
+    color: #313638;
+    line-height: 1.5;
   }
   :host([hidden]) {
         display: none;
       }
+  h2,
+  h3,
+  h4 {
+    font-family: "Palatino Linotype", "Book Antiqua", "Palatino", serif;
+    text-align: center;
+    color: #FF6B6B;
+    font-weight: normal;
+    font-size: 1.5em;
+    margin: 10px;
+  }
   label {
     display: block;
   }
-  input {
-    width: 50%;
+  input[type=text],
+  select {
+    width: 70%;
+    font-size: 1em;
+    padding: 8px;
+    margin: 8px auto 16px;
+    display: block;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
   }
+  input[type=radio] {
+    margin-right: 10px;
+  }
+  button {
+    display: block;
+    width: 70%;
+    font-size: 1em;
+    background-color: #FF6B6B;
+    color: white;
+    padding: 14px 20px;
+    margin: 8px auto;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+  }
+  button:hover {
+    background-color: #ff4c4c;
+}
   .hidden {
     display: none;
   }
@@ -28,7 +70,7 @@ template.innerHTML = /* html */`
 <input type="text" id="input-name" autofocus placeholder="Your cool nickname here...">
 <button id="buttonStart">Start Game!</button>
 </div>
-<div id="questionDiv">
+<div id="questionDiv" class="hidden">
 <h4>Timer: <span id="timer"></span></h4>
 <h3 id="question"></h3>
 <h4>Your answer:</h4>
@@ -36,10 +78,11 @@ template.innerHTML = /* html */`
 </div>
 <button id="buttonAnswer">Send Answer</button>
 </div>
-<div id="answerDiv">
+<div id="answerDiv" class="hidden">
 <h3 id="serverAnswer"></h3>
 <p id="customAnswer"></p>
-<button id="buttonRestart">Restart Game!</button>
+<p id="totalTime"></p>
+<button id="buttonRestart" class="hidden">Restart Game!</button>
 <button id="buttonGetQuestion">Next Question!</button>
 </div>
 `
@@ -62,20 +105,21 @@ class QuizGame extends window.HTMLElement {
   connectedCallback () {
     this.totalTime = 0
     this.apiURL = 'http://vhost3.lnu.se:20080/question/1'
+    this.response = null
     this.gameOver = false
     this.shadowRoot.querySelector('#buttonStart').addEventListener('click', () => {
       this.playerName = this.shadowRoot.querySelector('#input-name').value
-      this.getHighScores()
       this.getQuestion()
     }, { once: true }
     )
-    // Save refer to we can remove listener later.
+    // Save refer so we can remove listener later.
     this.boundSendAnswer = this.sendAnswer.bind(this)
     this.boundGetQuestion = this.getQuestion.bind(this)
 
     this.shadowRoot.querySelector('#buttonAnswer').addEventListener('click', this.boundSendAnswer)
     this.shadowRoot.querySelector('#buttonGetQuestion').addEventListener('click', this.boundGetQuestion)
     this.shadowRoot.querySelector('#buttonRestart').addEventListener('click', this.restartGame.bind(this), { once: true })
+    this._updateRendering()
   }
 
   disconnectedCallback () {
@@ -126,6 +170,10 @@ class QuizGame extends window.HTMLElement {
     console.log('TIMER STOPPED at: ' + this.timer)
   }
 
+  // query (selector) {
+  //   return this.shadowRoot.querySelector(selector)
+  // }
+
   async getQuestion () {
     let response = await window.fetch(this.apiURL)
     this.response = await response.json()
@@ -144,7 +192,7 @@ class QuizGame extends window.HTMLElement {
         }
       }
     } else {
-      let input = this.shadowRoot.querySelector('#input-text')
+      let input = this.shadowRoot.querySelector('#inputText')
       answer = input.value
     }
     return answer
@@ -173,55 +221,82 @@ class QuizGame extends window.HTMLElement {
         worstHighScore = score
       }
     })
-    if (this.totalTime <= worstHighScore) {
+    if (this.totalTime <= worstHighScore || window.localStorage.length < 5) {
       window.localStorage.setItem(this.playerName, this.totalTime)
     }
   }
 
   getHighScores () {
+    console.log('Highscores:')
     Object.keys(window.localStorage).forEach((key) => {
       console.log(`Name: ${key}, Time: ${window.localStorage.getItem(key)}`)
     })
   }
 
-  toggleHide (element) {
+  showElement (element) {
     if (element.classList.contains('hidden')) {
       element.classList.remove('hidden')
-    } else {
+    }
+  }
+
+  hideElement (element) {
+    if (!element.classList.contains('hidden')) {
       element.classList.add('hidden')
     }
   }
 
   _updateRendering () {
+    let startDiv = this.shadowRoot.querySelector('#startDiv')
     let questionDiv = this.shadowRoot.querySelector('#questionDiv')
+    let answerDiv = this.shadowRoot.querySelector('#answerDiv')
     let question = this.shadowRoot.querySelector('#question')
     let serverAnswer = this.shadowRoot.querySelector('#serverAnswer')
+    let customAnswer = this.shadowRoot.querySelector('#customAnswer')
+    let totalTime = this.shadowRoot.querySelector('#totalTime')
     let div = this.shadowRoot.querySelector('#inputDiv')
+    let buttonGetQuestion = this.shadowRoot.querySelector('#buttonGetQuestion')
+    let buttonRestart = this.shadowRoot.querySelector('#buttonRestart')
     div.innerHTML = ''
-    console.log(this.response)
-    if (this.gameOver) {
+    customAnswer.textContent = ''
+    totalTime.textContent = ''
+    this.hideElement(startDiv)
+    if (!this.response) {
+      this.hideElement(answerDiv)
+      this.showElement(startDiv)
+    } else if (this.gameOver) {
+      this.hideElement(questionDiv)
+      this.showElement(answerDiv)
+      this.hideElement(buttonGetQuestion)
+      this.showElement(buttonRestart)
       serverAnswer.textContent = this.response.message
-      console.log('GAME OVER!')
+      customAnswer.textContent = 'GAME OVER!'
     } else if (!this.apiURL) {
-      console.log('CONGRATULATIONS!')
-      console.log('YOUR TOTAL TIME: ' + this.totalTime)
+      this.hideElement(questionDiv)
+      this.showElement(answerDiv)
+      this.hideElement(buttonGetQuestion)
+      this.showElement(buttonRestart)
+      customAnswer.textContent = 'Congratulations! You passed the quiz!'
+      totalTime.textContent = `Your Total Time: ${this.totalTime / 1000} seconds`
       this.populateStorage()
     } else if (!this.response.question) {
+      this.hideElement(questionDiv)
+      this.hideElement(buttonRestart)
+      this.showElement(buttonGetQuestion)
+      this.showElement(answerDiv)
       serverAnswer.textContent = this.response.message
     } else if (this.response.question && !this.response.alternatives) {
-      console.log('THIS IS INTERPRETED AS A TEXT QUESTION!')
-      serverAnswer.textContent = ''
+      this.hideElement(answerDiv)
+      this.showElement(questionDiv)
       question.textContent = this.response.question
       let input = document.createElement('input')
       input.setAttribute('type', 'text')
-      input.setAttribute('id', 'input-text')
+      input.setAttribute('id', 'inputText')
       div.appendChild(input)
     } else if (this.response.alternatives) {
-      this.populateStorage()
-      console.log('THIS IS INTERPRETED AS AN ALTERNATIVES QUESTION!')
-      serverAnswer.textContent = ''
+      this.hideElement(answerDiv)
+      this.showElement(questionDiv)
       question.textContent = this.response.question
-      Object.keys(this.response.alternatives).forEach((key) => { // GÅR NEDAN ATT GÖRA ENKLARE MED EN TEMPLATE?
+      Object.keys(this.response.alternatives).forEach((key) => {
         let label = document.createElement('label')
         let radioButton = document.createElement('input')
         let text = document.createTextNode(this.response.alternatives[key])
